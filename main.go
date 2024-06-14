6c89e920bd5e2d245cc01f18629343712599b9db
Rubens García <4980620+rubensdev@users.noreply.github.com>
2024-06-14 17:14:49 +0100

    feat: added basic routing

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Hello World!\n")
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	router := httprouter.New()
+	router.GET("/", Index)
+
+	log.Println("Server listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
