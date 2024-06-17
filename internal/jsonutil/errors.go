cbd2c51abb55887c250758bdc48b3374ed5ea7b6
Rubens García <4980620+rubensdev@users.noreply.github.com>
2024-06-17 09:35:21 +0100

    add: json response helpers for managing bad requests

---
+++ internal/jsonutil/errors.go
@@ -0,0 +1,52 @@
+package jsonutil
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Output interface {
+	Print(v ...any)
+}
+
+type JSONResponse struct {
+	output Output
+}
+
+func NewJSONResponse(output Output) *JSONResponse {
+	return &JSONResponse{
+		output: output,
+	}
+}
+
+func (j JSONResponse) ServerErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	j.logError(r, err)
+
+	message := "the server has encountered a problem and could not process your request"
+	j.ErrorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (j JSONResponse) NotFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	j.ErrorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (j JSONResponse) MethodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	j.ErrorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (j JSONResponse) ErrorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	data := H{"error": message}
+
+	err := WriteJSON(w, status, data, nil)
+	if err != nil {
+		j.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (j JSONResponse) logError(r *http.Request, err error) {
+	// TODO: include http method and URL.
+	j.output.Print(err)
+}
